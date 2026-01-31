13a799cb7d6f05c378c8614a0e66b5a23cda62b5
deepesh-agrawal1 <adevagrawal@gmail.com>
2026-01-31 15:29:40 +0530

    uploding the backend

---
+++ src/main/java/org/hsbc/entity/PmsEntity.java
@@ -0,0 +1,116 @@
+package org.hsbc.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class PmsEntity
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String companyName;
+    String symbol;
+    int quantity;
+    double buyPrice;
+    double currentPrice;
+    double buyingValue;
+    String assetType;
+    public PmsEntity() {
+    }
+
+
+
+    public PmsEntity(long id, String companyName, String symbol, int quantity, double buyPrice, double currentPrice, double buyingValue, String assetType) {
+        this.id = id;
+        this.companyName = companyName;
+        this.symbol = symbol;
+        this.quantity = quantity;
+        this.buyPrice = buyPrice;
+        this.currentPrice = currentPrice;
+        this.buyingValue = buyingValue;
+        this.assetType = assetType;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getBuyPrice() {
+        return buyPrice;
+    }
+
+    public void setBuyPrice(double buyPrice) {
+        this.buyPrice = buyPrice;
+    }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public double getBuyingValue() {
+        return buyingValue;
+    }
+
+    public void setBuyingValue(double buyingValue) {
+        this.buyingValue = buyingValue;
+    }
+
+    public String getAssetType() {
+        return assetType;
+    }
+
+    public void setAssetType(String assetType) {
+        this.assetType = assetType;
+    }
+
+    @Override
+    public String toString() {
+        return "PmsEntity{" +
+                "id=" + id +
+                ", companyName='" + companyName + '\'' +
+                ", symbol='" + symbol + '\'' +
+                ", quantity=" + quantity +
+                ", buyPrice=" + buyPrice +
+                ", currentPrice=" + currentPrice +
+                ", buyingValue=" + buyingValue +
+                "assetType='" + assetType + '\'' +
+                '}';
+    }
+
+
+}
