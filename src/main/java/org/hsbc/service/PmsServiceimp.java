7e5d65d76d02335b1ecc3314f16c4c09d141dd2f
jayanth343 <jayanth.srinivasan99@gmail.com>
2026-02-05 09:06:34 +0000

    fix: Add missing import for HttpStatus in PmsServiceimp

--- src/main/java/org/hsbc/service/PmsServiceimp.java
+++ src/main/java/org/hsbc/service/PmsServiceimp.java
@@ -8,6 +8,7 @@ import org.hsbc.repo.PmsRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
